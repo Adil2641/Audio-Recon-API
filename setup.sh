ab1758e6692d0186bb8f0052e2d2810754c7d192
A Dil <adilhasan2641@gmail.com>
2025-04-12 08:48:50 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Create bin directory inside the project
-mkdir -p ./bin
+# Create bin directory for yt-dlp
+mkdir -p bin
 
-# Download yt-dlp binary locally
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./bin/yt-dlp
+# Download yt-dlp binary to local bin folder
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
 
-# Make it executable
-chmod +x ./bin/yt-dlp
+# Make yt-dlp executable
+chmod +x bin/yt-dlp
+
+# Disable auto-updates
+export YTDL_NO_UPDATE=1
